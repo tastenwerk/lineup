97e4a06a733c5fa041ba30d450bf029a73cf8109
David Reinisch <david.reinisch@tastenwerk.com>
2015-07-23 15:31:41 +0200

    image working

--- custom-meta-box.php
+++ custom-meta-box.php
@@ -150,16 +150,15 @@
   }
 
   function echo_image($field, $meta){
-    // $image = get_template_directory_uri().'/images/image.png';  
-    // echo '<span class="custom_default_image" style="display:none">'.$image.'</span>';
-    if ($meta) { $image = wp_get_attachment_image_src($meta, 'medium'); $image = $image[0]; }               
-      echo    '<input name="'.$field['id'].'"  class="custom_upload_image" value="'.$meta.'" />
-      <img src="'.$image.'" class="custom_preview_image" alt="" /><br />
-      <input class="custom_upload_image_button button" type="button" value="Choose Image" />
-      <small> <a href="#" class="custom_clear_image_button">Remove Image</a></small>
-      <br clear="all" /><span class="description">'.$field['desc'].'</span>';
-  }
-
+      $image = get_template_directory_uri().'/images/image.png';  
+      echo '<span class="custom_default_image" style="display:none">'.$image.'</span>';
+      if ($meta) { $image = wp_get_attachment_image_src($meta, 'medium'); $image = $image[0]; }               
+      echo    '<input name="'.$field['id'].'" type="hidden" class="custom_upload_image" value="'.$meta.'" />
+                  <img src="'.$image.'" class="custom_preview_image" alt="" /><br />
+                      <input class="custom_upload_image_button button" type="button" value="Choose Image" />
+                      <small> <a href="#" class="custom_clear_image_button">Remove Image</a></small>
+                      <br clear="all" /><span class="description">'.$field['desc'].'</span>';
+    }
   function echo_repeatable($field, $meta){
     echo '<a class="repeatable-add button" href="#">+</a>
           <ul id="'.$field['id'].'-repeatable" class="custom_repeatable">';
