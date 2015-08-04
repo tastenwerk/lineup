cec66e9a2112174dfca50f0cc4d6102b21975df4
david <david.reinisch@tastenwerk.com>
2015-08-05 00:09:46 +0200

    up

--- ess-feed.php
+++ ess-feed.php
@@ -3,7 +3,7 @@
 
 
   function add_ess_feed(){
-    
+
     $type = 'lineupentry';
     $args=array(
       'post_type' => $type,
@@ -17,45 +17,35 @@
     );
 
 
-    // print_r( $args );
-
     $my_query = null;
-    $my_query = new WP_Query($args);
+    $my_query = new WP_Query($args); 
+
+      
+    include(dirname( __FILE__)."/lib/ess/FeedWriter.php");
+
+    $current_url = get_site_url();
+    $feed_url   =  str_replace( 'localhost', '127.0.0.0', $current_url).'/feed/ess';
+    // echo $feed_url;
+    global $current_site;
+    // == Create the ESS Feed ================
+    $essFeed = new FeedWriter( 'en', array(
+        'title'     => 'Lineupentriesfeed',
+        'link'      => $feed_url,
+        'published' => $post->date,
+        'rights'    => get_bloginfo( 'name' )
+    ));
+
 
     while ($my_query->have_posts()) : 
       $my_query->the_post();
-      /************************************************************************************************
-       *
-       *  This complete exemple create an event feed that describe:
-       *
-       *  - A 2 hours Madonna concert that happend every years for three years at 9:30PM the 25th of Oct.
-       *  - It happends in a stadium in New York.
-       *  - This happening is defined with a category "concert" explained as "Rock music"
-       *  - Several specific TAGs are attached to improve SEO.
-       *  - The price is defined to $90 (with a URL for payment).
-       *  - The event can be free with a specific card accreditation.
-       *  - An image and a video is defined to give a face to the event.
-       *
-       ************************************************************************************************/
-
-      include(dirname( __FILE__)."/lib/ess/FeedWriter.php");
+      $post = get_post( get_the_ID() );
 
       global $wp;
         $current_url = home_url(add_query_arg(array(),$wp->request));
-      // print_r( $post );
 
 
-      $feed_url   = str_replace('localhost', '127.0.0.1', $current_url).'.ess';
-      $event_page = str_replace('localhost', '127.0.0.1', $current_url).'.html';
-
-      // == Create the ESS Feed ================
-      $essFeed = new FeedWriter( 'en', array(
-          'title'     => $post->post_title,
-          'link'      => $feed_url,
-          'published' => $post->date,
-          'rights'    => 'Mezzanintheater'
-      ));
-
+      $event_page = get_the_permalink();
+     
       $dates = get_post_meta($post->ID, 'lineupentry_dates', TRUE );
 
 
@@ -105,10 +95,10 @@
       // == Add the event to the Feed
       $essFeed->addItem( $newEvent );
 
+    endwhile;
+
       // == Display the ESS Feed generated.
       $essFeed->genarateFeed();
-
-    endwhile;
 
   }
 
