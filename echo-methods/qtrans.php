0efe4ed05d10d795578c1428e27ce7c3a4fb9c0d
David Reinisch <david.reinisch@tastenwerk.com>
2015-08-10 17:43:02 +0200

    up

---
+++ echo-methods/qtrans.php
@@ -0,0 +1,11 @@
+<?php
+
+  function localize( $string ){
+    //  TODO!
+    // echo qtrans_use( qtrans_getLanguage(), $string, false );
+    // echo split('[:]', $string);
+    // echo "HERE";
+    return substr( split('\[:', $string)[1], 3);
+  }
+
+?>
